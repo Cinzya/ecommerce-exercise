d85b971a6731928b5e7e9461e81536cab26e8659
Cinzya <cynti-ebert@web.de>
2024-10-25 01:26:11 +0200

    feat(CategorySidebar): change route on click

--- src/components/CategorySidebar.vue
+++ src/components/CategorySidebar.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
 import { useCategoryStore } from '@/stores/category'
+
 const categoryStore = useCategoryStore()
 </script>
 
 <template>
   <h2 class="mb-4 text-base font-medium uppercase text-gray-900">Category</h2>
 
-  <div
+  <label
     v-for="category in categoryStore.$state.categories"
     :key="category.id"
-    class="mb-3 flex items-center"
+    class="mb-3 flex items-center text-sm font-medium text-gray-900"
   >
     <input
       id="default-radio-1"
       type="radio"
-      value=""
+      @input="
+        $router.push({
+          name: 'category',
+          params: { category: category.id.toString() },
+        })
+      "
+      :checked="category.id === parseInt($route.params.category as string)"
       name="default-radio"
-      class="h-4 w-4 border-gray-300 bg-gray-100 accent-primary-500 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-600"
+      class="mr-2 h-4 w-4 border-gray-300 bg-gray-100 accent-primary-500 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-600"
     />
-    <label
-      for="default-radio-1"
-      class="ms-2 text-sm font-medium text-gray-900"
-      >{{ category.name }}</label
-    >
-  </div>
+    {{ category.name }}
+  </label>
 </template>
