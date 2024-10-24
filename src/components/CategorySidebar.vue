f5ac2830da0487e9550358055f37a71e40d61590
Cinzya <cynti-ebert@web.de>
2024-10-24 23:11:43 +0200

    feat: create category sidebar base

---
+++ src/components/CategorySidebar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useCategoryStore } from '@/stores/category'
+const { categories } = useCategoryStore()
+</script>
+
+<template>
+  <h2 class="mb-4 text-base font-medium uppercase text-gray-900">Category</h2>
+
+  <div
+    v-for="category in categories"
+    :key="category.name"
+    class="mb-3 flex items-center"
+  >
+    <input
+      id="default-radio-1"
+      type="radio"
+      value=""
+      name="default-radio"
+      class="h-4 w-4 border-gray-300 bg-gray-100 accent-primary-500 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-600"
+    />
+    <label
+      for="default-radio-1"
+      class="ms-2 text-sm font-medium text-gray-900"
+      >{{ category.name }}</label
+    >
+  </div>
+</template>
