b5eb9138b1ff53c2e09a4063c053086adbef4b74
Cinzya <cynti-ebert@web.de>
2024-10-24 23:12:15 +0200

    feat(CategoryStore): add IDs to categories

--- src/stores/category.ts
+++ src/stores/category.ts
@@ -15,50 +15,56 @@ interface CategoryStore {
 
 export const useCategoryStore = defineStore('category', () => {
   const categories = ref([
-    {
-      name: 'Computer & Laptop',
-      subcategories: [
-        'Laptop',
-        'Desktop',
-        'Computer Accessories',
-        'Storage Devices',
-        'Networking Devices',
-        'Software',
-      ],
-    },
-    {
-      name: 'Computer Accessories',
-      subcategories: [
-        'Laptop Bags',
-        'Mouse',
-        'Keyboard',
-        'Headphones',
-        'Webcams',
-        'Speakers',
-      ],
-    },
-    {
-      name: 'Phones & Tablets',
-      subcategories: [
-        'Smartphones',
-        'Tablets',
-        'Phone Accessories',
-        'Tablet Accessories',
-        'Smartwatches',
-        'Fitness Bands',
-      ],
-    },
-    {
-      name: 'Headphones',
-      subcategories: [
-        'In-Ear Headphones',
-        'On-Ear Headphones',
-        'Over-Ear Headphones',
-        'Wireless Headphones',
-        'Noise Cancelling Headphones',
-        'Sports Headphones',
-      ],
-    },
+    ref([
+      {
+        id: 1,
+        name: 'Computer & Laptop',
+        subcategories: [
+          'Laptop',
+          'Desktop',
+          'Computer Accessories',
+          'Storage Devices',
+          'Networking Devices',
+          'Software',
+        ],
+      },
+      {
+        id: 2,
+        name: 'Computer Accessories',
+        subcategories: [
+          'Laptop Bags',
+          'Mouse',
+          'Keyboard',
+          'Headphones',
+          'Webcams',
+          'Speakers',
+        ],
+      },
+      {
+        id: 3,
+        name: 'Phones & Tablets',
+        subcategories: [
+          'Smartphones',
+          'Tablets',
+          'Phone Accessories',
+          'Tablet Accessories',
+          'Smartwatches',
+          'Fitness Bands',
+        ],
+      },
+      {
+        id: 4,
+        name: 'Headphones',
+        subcategories: [
+          'In-Ear Headphones',
+          'On-Ear Headphones',
+          'Over-Ear Headphones',
+          'Wireless Headphones',
+          'Noise Cancelling Headphones',
+          'Sports Headphones',
+        ],
+      },
+    ]),
   ])
   const selectedCategory = ref<Subcategory | null>(null)
   const selectedCategoryIndex = ref<number | null>(null)
