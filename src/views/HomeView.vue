8a503ce7d4edd5a59f3105ad0019a2f4335b5988
Cinzya <cynti-ebert@web.de>
2024-10-25 14:38:19 +0200

    feat(HomeView): add filter toggle button in sidebar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import CategorySidebar from '@/components/CategorySidebar.vue'
+const isFilterOpen = ref(false)
 </script>
 
 <template>
-  <div class="mx-auto flex max-w-screen-xl px-5">
+  <div class="mx-auto flex max-w-screen-xl flex-col px-5 lg:flex-row">
     <aside>
+      <button
+        @click="isFilterOpen = !isFilterOpen"
+        class="rounded-3px border-2 border-primary-500 bg-white p-2 px-5 text-base font-bold uppercase text-primary-500 lg:hidden"
+      >
+        Filters
+      </button>
       <CategorySidebar />
     </aside>
     <main>
