6e809dbca135af94347a5324d9874dbeab89de82
Cinzya <cynti-ebert@web.de>
2024-10-25 15:56:42 +0200

    refactor(FilterModal): create component for filter modal

---
+++ src/components/FilterModal.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import CategorySidebar from './CategorySidebar.vue'
+const isFilterOpen = ref(false)
+const isMobileFilterOpen = computed(
+  () => isFilterOpen.value && window.innerWidth < 1024,
+)
+</script>
+<template>
+  <button
+    @click="isFilterOpen = !isFilterOpen"
+    class="rounded-3px mt-5 border-2 border-primary-500 bg-white p-2 px-5 text-base font-bold uppercase text-primary-500 lg:hidden"
+  >
+    Filters
+  </button>
+  <!-- Modal Backdrop -->
+  <div
+    v-if="isMobileFilterOpen"
+    @click.self="isFilterOpen = false"
+    class="pointer—events—none inset—0 cursor—pointer transition—att duration—20 ease—in—out translate—y-0 fixed left-0 top-0 flex h-screen w-screen items-center justify-center overflow-hidden overscroll-contain bg-slate-700/30"
+  >
+    <!-- Modal -->
+    <div
+      class="max-h-[calc(100vh-5rem)] w-screen overscroll-contain rounded-[3px] bg-white px-5"
+    >
+      <div class="my-4 flex items-center justify-between">
+        Filter
+        <button @click="isFilterOpen = false"><PhX :size="20" /></button>
+      </div>
+      <CategorySidebar />
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,14 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
 import CategorySidebar from '@/components/CategorySidebar.vue'
-const isFilterOpen = ref(false)
-const isMobileFilterOpen = computed(
-  () => isFilterOpen.value && window.innerWidth < 1024,
-)
+import FilterModal from '@/components/FilterModal.vue'
 </script>
 
 <template>
-  <div class="mx-auto flex max-w-screen-xl flex-col px-5 lg:flex-row">
-    <aside>
-      <button
-        @click="isFilterOpen = !isFilterOpen"
-        class="rounded-3px mt-5 border-2 border-primary-500 bg-white p-2 px-5 text-base font-bold uppercase text-primary-500 lg:hidden"
-      >
-        Filters
-      </button>
-      <!-- Modal Backdrop -->
-      <div
-        v-if="isMobileFilterOpen"
-        @click.self="isFilterOpen = false"
-        class="pointer—events—none inset—0 cursor—pointer transition—att duration—20 ease—in—out translate—y-0 fixed left-0 top-0 flex h-screen w-screen items-center justify-center overflow-hidden overscroll-contain bg-slate-700/30"
-      >
-        <!-- Modal -->
-        <div
-          class="max-h-[calc(100vh-5rem)] w-screen overscroll-contain rounded-[3px] bg-white px-5"
-        >
-          <div class="my-4 flex items-center justify-between lg:hidden">
-            Filter
-            <button @click="isFilterOpen = false"><PhX :size="20" /></button>
-          </div>
-          <CategorySidebar />
-        </div>
-      </div>
+  <div class="mx-auto flex max-w-screen-xl flex-col px-5 pt-4 lg:flex-row">
+    <aside class="hidden lg:block">
+      <CategorySidebar />
     </aside>
+    <FilterModal class="lg:hidden" />
     <main>
       <router-view />
     </main>
