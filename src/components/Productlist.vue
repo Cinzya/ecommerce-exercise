c9f11eece3286151b86bf709c064f4cdc57993fe
Cinzya <cynti-ebert@web.de>
2024-10-25 23:16:33 +0200

    feat(ProductList): add breakpoints to grid layout for responsive design

--- src/components/Productlist.vue
+++ src/components/Productlist.vue
@@ -10,7 +10,9 @@ const data = computed(() =>
 )
 </script>
 <template>
-  <div class="grid grid-cols-4 gap-4">
+  <div
+    class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4"
+  >
     <!-- Card -->
     <div
       v-for="article in data"
