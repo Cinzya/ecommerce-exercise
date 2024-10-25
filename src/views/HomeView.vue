fb26715155ee348f85a4332347dd5219fbb84c7f
Cinzya <cynti-ebert@web.de>
2024-10-26 00:29:51 +0200

    feat(HomeView): add PriceRange component to CategorySidebar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import CategorySidebar from '@/components/CategorySidebar.vue'
 import FilterModal from '@/components/FilterModal.vue'
+import PriceRange from '@/components/PriceRange.vue'
 </script>
 
 <template>
   <div class="mx-auto flex max-w-screen-xl flex-col px-5 pt-4 lg:flex-row">
     <aside class="mr-6 hidden w-[312px] shrink-0 lg:block">
       <CategorySidebar />
+      <PriceRange />
     </aside>
     <FilterModal class="lg:hidden" />
     <main class="w-full">
