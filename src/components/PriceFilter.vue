6be1d6ab3d715a31616821ca94dc941b40746865
Cinzya <cynti-ebert@web.de>
2024-10-26 21:42:19 +0200

    fix(PriceFilter): minimum value for €300 to €500 range

--- src/components/PriceFilter.vue
+++ src/components/PriceFilter.vue
@@ -5,7 +5,7 @@ const predefinedRanges = [
   { label: 'Under €20', min: 0, max: 20 },
   { label: '€25 to €100', min: 25, max: 100 },
   { label: '€100 to €300', min: 100, max: 300 },
-  { label: '€300 to €500', min: 200, max: 500 },
+  { label: '€300 to €500', min: 300, max: 500 },
   { label: '€500 to €1000', min: 500, max: 1000 },
   { label: '€1,000 to €10,000', min: 1000, max: 10000 },
 ]
