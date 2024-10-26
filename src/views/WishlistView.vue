7f1a7eff9cbf4bbdd59fb5658211905e1cd5d21c
Cinzya <cynti-ebert@web.de>
2024-10-26 23:41:55 +0200

    feat(Wishlist): add wishlist table

--- src/views/WishlistView.vue
+++ src/views/WishlistView.vue
@@ -1,3 +1,36 @@
 <template>
-  <div>Wishlist</div>
+  <div
+    class="mx-auto flex max-w-screen-xl flex-col overflow-hidden px-5 pt-4 lg:flex-row"
+  >
+    <table class="w-full table-auto rounded border border-gray-100 bg-white">
+      <thead>
+        <tr class="text-left">
+          <th
+            class="px-6 py-5 text-lg font-medium leading-normal text-gray-900"
+          >
+            Wishlist
+          </th>
+        </tr>
+      </thead>
+      <thead>
+        <tr
+          class="border-b border-t border-gray-100 bg-gray-50 text-left text-xs font-medium uppercase text-gray-700"
+        >
+          <th class="px-6 py-5">Products</th>
+          <th>Price</th>
+          <th>Stock Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="px-6 py-5">
+            The Sliding Mr. Bones (Next Stop, Pottersville)
+          </td>
+          <td>Malcolm Lockyer</td>
+          <td>1961</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
