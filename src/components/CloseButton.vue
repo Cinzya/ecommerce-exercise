b731ae9e94bc847e05ff05e6a750eb6da9a29649
Cinzya <cynti-ebert@web.de>
2024-10-28 00:16:17 +0100

    feat(CloseButton): allow custom class prop for button styling

--- src/components/CloseButton.vue
+++ src/components/CloseButton.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string
+}>()
+</script>
 <template>
-  <button class="ml-6 text-gray-400">
+  <button class="ml-6 text-gray-400" :class="props.class">
     <PhXCircle :size="24" />
   </button>
 </template>
