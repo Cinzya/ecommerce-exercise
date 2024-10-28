dda78700ac9b258c2cc49676201444573b828827
Cinzya <cynti-ebert@web.de>
2024-10-28 02:39:58 +0100

    refactor(FilterModal): use teleport

--- src/components/FilterModal.vue
+++ src/components/FilterModal.vue
@@ -1,37 +1,51 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
+import { useModalStore } from '@/stores/modal'
 import CategorySidebar from './CategorySidebar.vue'
 import PriceFilter from './PriceFilter.vue'
 import BrandFilter from './BrandFilter.vue'
-const isFilterOpen = ref(false)
+const store = useModalStore()
 const isMobileFilterOpen = computed(
-  () => isFilterOpen.value && window.innerWidth < 1024,
+  () => store.$state.isOpen && window.innerWidth < 1024,
 )
+const toggleModal = () => {
+  if (store.isOpen) {
+    store.close()
+  } else {
+    store.open()
+  }
+}
 </script>
 <template>
   <button
-    @click="isFilterOpen = !isFilterOpen"
-    class="rounded-3px mt-5 border-2 border-primary-500 bg-white p-2 px-5 text-base font-bold uppercase text-primary-500 lg:hidden"
+    @click="toggleModal"
+    class="rounded-3px my-5 border-2 border-primary-500 bg-white p-2 px-5 text-base font-bold uppercase text-primary-500 lg:hidden"
   >
     Filters
   </button>
-  <!-- Modal Backdrop -->
-  <div
-    v-if="isMobileFilterOpen"
-    @click.self="isFilterOpen = false"
-    class="modal pointer—events—none inset—0 cursor—pointer transition—att duration—20 ease—in—out translate—y-0 fixed left-0 top-0 z-10 flex h-screen w-screen items-end justify-center overflow-hidden overscroll-contain bg-slate-700/30"
-  >
-    <!-- Modal -->
+  <Teleport to="#modal">
+    <!-- Modal Backdrop -->
     <div
-      class="max-h-[calc(100vh-5rem)] w-screen overscroll-contain rounded-[3px] bg-white px-5"
+      v-if="isMobileFilterOpen"
+      @click.self="store.close()"
+      class="modal pointer—events—none inset—0 cursor—pointer transition—att duration—20 ease—in—out translate—y-0 fixed left-0 top-0 z-50 flex h-screen w-screen items-end justify-center overflow-hidden overscroll-contain bg-slate-700/30"
     >
-      <div class="my-4 flex items-center justify-between">
-        Filter
-        <button @click="isFilterOpen = false"><PhX :size="20" /></button>
+      <!-- Modal -->
+      <div
+        class="max-h-[calc(100vh-5rem)] w-screen overscroll-contain rounded-[3px] bg-white"
+      >
+        <div
+          class="my-4 flex items-center justify-between border-b border-gray-100 px-5 pb-4"
+        >
+          Filter
+          <button @click="store.close()"><PhX :size="20" /></button>
+        </div>
+        <div class="max-h-[calc(100vh-10rem)] overflow-y-scroll px-5">
+          <CategorySidebar />
+          <PriceFilter />
+          <BrandFilter />
+        </div>
       </div>
-      <CategorySidebar />
-      <PriceFilter />
-      <BrandFilter />
     </div>
-  </div>
+  </Teleport>
 </template>
